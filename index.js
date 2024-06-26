360b0e91e2e0f6b2fe5f06cc2182391cd6538695
Areesha-Noor2 <163730958+Areesha-Noor2@users.noreply.github.com>
2024-06-26 17:57:35 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,150 @@
+document.write(
+    "<body style='background-color:#333333;' " +
+      "</body>"
+  );
+
+var userWeather = prompt("Enter Your Area's Weather  " + "(including: cloudy, winter, sunny, summer, windy, rainy, spring, stormy, cold, drizzle, fog,lightning, monsoon,snowy, hot.)") 
+
+if( userWeather ==="cloudy"){
+    document.write(
+        "<div class='card mx-auto ' style='width: 19rem; margin-top:13%; box-shadow: rgba(0, 0, 0, 0.35) 0px 0px 15px; border:none;'> " +
+        "<img src='image-1.jpg' class='card-img-top'> "+
+        "<div class='card-body'>" +
+          "<h5 class='card-title' style = 'text-align: center'>" + "Cloudy Weather" + "</h5>" +
+        "</div>" +
+        "</div>"
+    )
+}else if(userWeather === "winter"){
+    document.write(
+        "<div class='card mx-auto' style='width: 19rem; margin-top:13%; box-shadow: rgba(0, 0, 0, 0.35) 0px 0px 15px; border:none; '> " +
+        "<img src='image-2.webp' class='card-img-top'> "+
+        "<div class='card-body'>" +
+          "<h5 class='card-title' style = 'text-align: center'>" + "Winter Weather" + "</h5>" +
+        "</div>" +
+        "</div>"
+    )
+}else if(userWeather ==="sunny"){
+    document.write(
+        "<div class='card mx-auto' style='width: 19rem; margin-top:13%; box-shadow: rgba(0, 0, 0, 0.35) 0px 0px 15px; border:none;'> " +
+        "<img src='image-3.jpeg' class='card-img-top'> "+
+        "<div class='card-body'>" +
+          "<h5 class='card-title' style = 'text-align: center'>" + "Sunny Weather" + "</h5>" +
+        "</div>" +
+        "</div>"
+    )
+}else if(userWeather ==="summer"){
+    document.write(
+        "<div class='card mx-auto' style='width: 19rem; margin-top:13%; box-shadow: rgba(0, 0, 0, 0.35) 0px 0px 15px; border:none;'> " +
+        "<img src='image-4.jpeg' class='card-img-top'> "+
+        "<div class='card-body'>" +
+          "<h5 class='card-title' style = 'text-align: center'>" + "Summer Weather" + "</h5>" +
+        "</div>" +
+        "</div>"
+    )
+}else if(userWeather ==="windy"){
+    document.write(
+        "<div class='card mx-auto' style='width: 19rem; margin-top:13%; box-shadow: rgba(0, 0, 0, 0.35) 0px 0px 15px; border:none;'> " +
+        "<img src='image-5.jpeg' class='card-img-top'> "+
+        "<div class='card-body'>" +
+          "<h5 class='card-title' style = 'text-align: center'>" + "Windy Weather" + "</h5>" +
+        "</div>" +
+        "</div>"
+    )
+}else if(userWeather ==="rainy"){
+    document.write(
+        "<div class='card mx-auto' style='width: 19rem; margin-top:13%; box-shadow: rgba(0, 0, 0, 0.35) 0px 0px 15px; border:none;'> " +
+        "<img src='image-6.jpg' class='card-img-top'> "+
+        "<div class='card-body'>" +
+          "<h5 class='card-title' style = 'text-align: center'>" + "Rainy Weather" + "</h5>" +
+        "</div>" +
+        "</div>"
+    )
+}else if(userWeather ==="spring"){
+    document.write(
+        "<div class='card mx-auto' style='width: 19rem; margin-top:13%; box-shadow: rgba(0, 0, 0, 0.35) 0px 0px 15px; border:none;'> " +
+        "<img src='image-7.webp' class='card-img-top'> "+
+        "<div class='card-body'>" +
+          "<h5 class='card-title' style = 'text-align: center'>" + "Spring Weather" + "</h5>" +
+        "</div>" +
+        "</div>"
+    )
+}else if(userWeather ==="stormy"){
+    document.write(
+        "<div class='card mx-auto' style='width: 19rem; margin-top:13%; box-shadow: rgba(0, 0, 0, 0.35) 0px 0px 15px; border:none;'> " +
+        "<img src='image-8.jpg' class='card-img-top'> "+
+        "<div class='card-body'>" +
+          "<h5 class='card-title' style = 'text-align: center'>" + "Stormy Weather" + "</h5>" +
+        "</div>" +
+        "</div>"
+    )
+}else if(userWeather ==="cold"){
+    document.write(
+        "<div class='card mx-auto' style='width: 19rem; margin-top:13%; box-shadow: rgba(0, 0, 0, 0.35) 0px 0px 15px; border:none;'> " +
+        "<img src='image-9.webp' class='card-img-top'> "+
+        "<div class='card-body'>" +
+          "<h5 class='card-title' style = 'text-align: center'>" + "Cold Weather" + "</h5>" +
+        "</div>" +
+        "</div>"
+    )
+}else if(userWeather ==="drizzle"){
+    document.write(
+        "<div class='card mx-auto' style='width: 19rem; margin-top:13%; box-shadow: rgba(0, 0, 0, 0.35) 0px 0px 15px; border:none;'> " +
+        "<img src='image-10.webp' class='card-img-top'> "+
+        "<div class='card-body'>" +
+          "<h5 class='card-title' style = 'text-align: center'>" + "Drizzle Weather" + "</h5>" +
+        "</div>" +
+        "</div>"
+    )
+}else if(userWeather ==="fog"){
+    document.write(
+        "<div class='card mx-auto' style='width: 19rem; margin-top:13%; box-shadow: rgba(0, 0, 0, 0.35) 0px 0px 15px; border:none;'> " +
+        "<img src='image-11.webp' class='card-img-top'> "+
+        "<div class='card-body'>" +
+          "<h5 class='card-title' style = 'text-align: center'>" + "Foggy Weather" + "</h5>" +
+        "</div>" +
+        "</div>"
+    )
+
+}else if(userWeather ==="lightning"){
+    document.write(
+        "<div class='card mx-auto' style='width: 19rem; margin-top:13%; box-shadow: rgba(0, 0, 0, 0.35) 0px 0px 15px; border:none;'> " +
+        "<img src='image-12.jpeg' class='card-img-top'> "+
+        "<div class='card-body'>" +
+          "<h5 class='card-title' style = 'text-align: center'>" + "Lightning Weather" + "</h5>" +
+        "</div>" +
+        "</div>"
+    )
+}else if(userWeather ==="monsoon"){
+    document.write(
+        "<div class='card mx-auto' style='width: 19rem; margin-top:13%; box-shadow: rgba(0, 0, 0, 0.35) 0px 0px 15px; border:none;'> " +
+        "<img src='image-13.jpeg' class='card-img-top'> "+
+        "<div class='card-body'>" +
+          "<h5 class='card-title' style = 'text-align: center'>" + "Monsoon Weather" + "</h5>" +
+        "</div>" +
+        "</div>"
+    )
+}else if(userWeather ==="snowy"){
+    document.write(
+        "<div class='card mx-auto' style='width: 19rem; margin-top:13%; box-shadow: rgba(0, 0, 0, 0.35) 0px 0px 15px; border:none;'> " +
+        "<img src='images-14.jpeg' class='card-img-top'> "+
+        "<div class='card-body'>" +
+          "<h5 class='card-title' style = 'text-align: center'>" + "Snowy Weather" + "</h5>" +
+        "</div>" +
+        "</div>"
+    )
+}else if(userWeather ==="hot"){
+    document.write(
+        "<div class='card mx-auto' style='width: 19rem; margin-top:13%; box-shadow: rgba(0, 0, 0, 0.35) 0px 0px 15px; border:none;'> " +
+        "<img src='image-15.jpg' class='card-img-top'> "+
+        "<div class='card-body'>" +
+          "<h5 class='card-title' style = 'text-align: center'>" + "Hot Weather" + "</h5>" +
+        "</div>" +
+        "</div>"
+    )
+}else{
+    document.write(
+        "<h1 style='text-align:center; margin-top:20%; color: white'>" +
+        "Dont Waste My Time??"+
+        "</h1>"
+    )
+}
